c2e245bde0baca40ae0e320b7ba2fe9a2150c28e
Franzé Jr <franzejr@gmail.com>
2012-11-29 20:20:30 -0300

    [#master] Adding csv file and starting to create the test unit for the StayPointDetection

--- MOAPStop/test/unit/src/arida/ufc/br/moap/stop/imp/StayPointDetectionTest.java
+++ MOAPStop/test/unit/src/arida/ufc/br/moap/stop/imp/StayPointDetectionTest.java
@@ -1,12 +1,13 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package arida.ufc.br.moap.stop.imp;
 
+import arida.ufc.br.moap.core.beans.MovingObject;
+import arida.ufc.br.moap.core.beans.Trajectory;
 import arida.ufc.br.moap.core.imp.Parameters;
+import arida.ufc.br.moap.datamodelapi.imp.TrajectoryModelImpl;
 import arida.ufc.br.moap.datamodelapi.spi.ITrajectoryModel;
+import arida.ufc.br.moap.importer.csv.imp.RawTrajectoryCSVImporter;
 import java.util.List;
+import org.joda.time.Interval;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -54,18 +55,48 @@ public class StayPointDetectionTest {
     }
 
     /**
-     * Test of execute method, of class StayPointDetection.
+     * The execute method execute the StayPoint Algorithm
      */
     @Test
     public void testExecute() {
-        System.out.println("execute");
-        ITrajectoryModel data = null;
-        Parameters parameters = null;
-        StayPointDetection instance = new StayPointDetection();
-        ITrajectoryModel expResult = null;
-        ITrajectoryModel result = instance.execute(data, parameters);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        //Importing the file
+        //There is a file inside the folder data
+        RawTrajectoryCSVImporter importer = new RawTrajectoryCSVImporter();
+        TrajectoryModelImpl model = new TrajectoryModelImpl();
+        Parameters parameters = new Parameters();
+        //You must to set the path
+        parameters.addParam(RawTrajectoryCSVImporter.PARAMETER_FILE, "/Users/franzejr/NetBeansProjects/moap/MOAPStop/test/unit/src/data/paradas.csv");
+        try {
+            importer.buildImport(model, parameters);
+            System.out.println("Trajectories: "+model.getTrajectoryCount());
+            System.out.println("Moving Object Count: "+model.getMovingObjectCount());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+        //Adding parameters to the algorithm
+        parameters.addParam("timeThreshold", 300000.0);
+        parameters.addParam("spatialThreshold", 0.003);
+        
+        //Initializing a StayPoint Algorithm
+        StayPointDetection s = new StayPointDetection();
+        ITrajectoryModel<StayPoint,Interval> result = s.execute(model, parameters);
+        System.out.println("RESULTADO:" +result.getTrajectoryCount());
+        
+        
+        //Here we can use the model as we wish
+        for(MovingObject mo : result.getMovingObjects()){
+            System.out.println("MOVING OBJECT ID:"+mo.toString());
+        }
+        
+        
+        for (Trajectory t : result.getTrajectories()) {
+            System.out.println("STAY POINT: " + t.getPoint(0));
+            System.out.println("PONTOS:"+t.getPoints());
+            System.out.println(t.getMovingObject().getId());
+
+        }
+
     }
 }
