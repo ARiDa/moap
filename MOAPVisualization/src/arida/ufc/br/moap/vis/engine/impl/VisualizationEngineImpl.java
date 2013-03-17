a359e766adc05882e16175a3717334c679e60e71
Franzé Jr <franzejr@gmail.com>
2013-03-17 12:34:25 -0300

    Melhorias na Classe VisualizationEngineImpl

--- MOAPVisualization/src/arida/ufc/br/moap/vis/engine/impl/VisualizationEngineImpl.java
+++ MOAPVisualization/src/arida/ufc/br/moap/vis/engine/impl/VisualizationEngineImpl.java
@@ -1,5 +1,6 @@
 package arida.ufc.br.moap.vis.engine.impl;
 
+import java.awt.Polygon;
 import java.awt.Shape;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
@@ -16,32 +17,95 @@ public class VisualizationEngineImpl implements IVisualizationEngine {
 
 	@Override
 	public Shape getShape(JXMapViewer viewer, IFeaturable feature) {
-		// TODO Auto-generated method stub
-		
-		GeometryType type = feature.getFeature().getGeometry().getGeometryType();
+
+		GeometryType type = feature.getFeature().getGeometry()
+				.getGeometryType();
 		switch (type) {
 		case POINT:
-			return getPoint(viewer,feature);
+			return getPoint(viewer, feature);
+		case LINESTRING:
+			return getLineString(viewer, feature);
+		case POLYGON:
+			return getPolygon(viewer, feature);
 		default:
 			break;
 		}
-		
+
 		return null;
 	}
 	
-	private Shape getPoint(JXMapViewer viewer,IFeaturable feature){
+	/*
+	 * Return a LineString Shape.
+	 * 
+	 * @param viewer
+	 * @param feature
+	 * @return lineString
+	 */
+	private Shape getLineString(JXMapViewer viewer, IFeaturable feature) {
+		/*
+		 * This class has the same behavior than {@link Polygon2D}, except that
+		 * the figure is not closed.
+		 */
+		Polyline2D lineString = new Polyline2D();
 		IGeometry geom = feature.getFeature().getGeometry();
-		
-		double[][] coord = geom.getCoordinates();
-		
-		Point2D pt = viewer.getTileFactory().geoToPixel(new GeoPosition(coord[0][0], coord[0][1]), viewer.getZoom());
-			
-		Ellipse2D e = new Ellipse2D.Double(pt.getX(), pt.getY(), 1.0, 1.0);
-		
-		return e;
-		
+		Point2D pt = null;
+		double[][] coords = geom.getCoordinates();
+
+		for (int x = 1; x < coords.length; x++) {
+			for (int y = 1; y < coords.length; y++) {
+				pt = viewer.getTileFactory().geoToPixel(new GeoPosition(coords[x][y], coords[x][y]), viewer.getZoom());
+				lineString.addPoint(pt);
+			}
+		}
+
+		return lineString;
 	}
+	/*
+	 * Return a Polygon Shape
+	 * 
+	 * @param viewer
+	 * @param feature
+	 * 
+	 * @return polygon
+	 */
+	private Shape getPolygon(JXMapViewer viewer, IFeaturable feature) {
+		IGeometry geom = feature.getFeature().getGeometry();
+		Polygon polygon = new Polygon();
+		Point2D pt = null;
+		double[][] coords = geom.getCoordinates();
 
+		for (int x = 0; x < coords.length; x++) {
+			for (int y = 0; y < coords.length; y++) {
+				pt = viewer.getTileFactory().geoToPixel(
+						new GeoPosition(coords[x][y], coords[x][y]),
+						viewer.getZoom());
+				polygon.addPoint((int) pt.getX(), (int) pt.getY());
 
+			}
+		}
+
+		return polygon;
+	}
+	/*
+	 * Return a Point Shape
+	 * 
+	 * @param viewer
+	 * @param feature
+	 * 
+	 * @return point
+	 */
+	private Shape getPoint(JXMapViewer viewer, IFeaturable feature) {
+		IGeometry geom = feature.getFeature().getGeometry();
+
+		double[][] coord = geom.getCoordinates();
+
+		Point2D pt = viewer.getTileFactory().geoToPixel(
+				new GeoPosition(coord[0][0], coord[0][1]), viewer.getZoom());
+
+		Ellipse2D e = new Ellipse2D.Double(pt.getX(), pt.getY(), 1.0, 1.0);
+
+		return e;
+
+	}
 
 }
